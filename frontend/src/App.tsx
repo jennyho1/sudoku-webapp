cc9a1cc55fe753e2cdf958d63fdf7c2bba83e064
Jenny Ho <jenny.ho@mail.utoronto.ca>
2024-11-06 21:27:54 -0500

    added header and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 import "./styles/Board.css";
 import "./styles/Sidebar.css";
 import Game from "./components/Game";
 
 function App() {
-  return <Game />;
+  return (
+    <>
+      <header>
+        <h1>Sudoku</h1>
+      </header>
+      <Game />
+    </>
+  );
 }
 
 export default App;
